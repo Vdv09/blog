6c8ef3a32a0bb18d1f71ecf566ac0cda3ff148a2
vdv09 <m.bugryshev@gmail.com>
2022-12-31 17:35:29 +0500

    Added login fo users

--- app/routes.py
+++ app/routes.py
@@ -3,21 +3,28 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
-
+from flask_login import current_user, login_user
+from app.models import User
 
 @app.route("/")
 @app.route("/index")
-
 def index():
-    user = {"username": "Miguel"}
-    return render_template("index.html", title = "Home", user = user)
+    return render_template("index.html", title = "Home", user = current_user)
 
 @app.route("/login", methods = ["GET", "POST"])
-
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
 
     if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash("Incorrect username or password")
+            return redirect(url_for("login"))
+
+        login_user(user, remember=form.remember_me.data)
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for("index"))
