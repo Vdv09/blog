ba775a33f0cc3dca3b3e8f5330d38f3741f819ae
vdv09 <m.bugryshev@gmail.com>
2022-12-31 22:09:04 +0500

    Change database and added posts

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,17 @@ from werkzeug.urls import url_parse
 @app.route("/index")
 @login_required
 def index():
-    return render_template("index.html", title = "Home", user = current_user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", title = "Home", posts = posts)
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
