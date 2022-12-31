6d2d0d934fb3e08243f2d6740236e79e27ef08c8
vdv09 <m.bugryshev@gmail.com>
2022-12-31 15:04:17 +0500

    Change password to password_hash

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
-    password = db.Column(db.String(300))
+    password_hash = db.Column(db.String(128))
 
     posts = db.relationship("Post", backref = "author", lazy = "dynamic")
 
